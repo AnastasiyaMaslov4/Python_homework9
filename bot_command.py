6f569f1542618d96d0dfb93ff6047d29de752bc6
AnastasiyaM <anastasiya.smirnova96@yandex.ru>
2022-10-26 13:19:25 +0300

    Bugs fixed

--- bot_command.py
+++ bot_command.py
@@ -11,17 +11,21 @@ user_choise = None
 
 
 def start_game(update: Update, context: CallbackContext):
+    global used_cell
+    global game_board
+    used_cell = []
     game_board = list(range(1,10))
     update.message.reply_text(f"------------------- \n | {game_board[0]} | {game_board[1]} | {game_board[2]} | \n | {game_board[3]} | {game_board[4]} | {game_board[5]} | \n | {game_board[6]} | {game_board[7]} | {game_board[8]} | \n -------------------\n Куда поставишь Х? Отправь номер клетки")
 
 
 def x_choise(update: Update, context: CallbackContext):
     global user_choise
+    global game_board
     user_choise = int(update.message.text)
     check(update)
-    who_wins(update, game_board)
-    bot_turn(update)
-    who_wins(update, game_board)
+    if not (who_wins(update, game_board)):
+        bot_turn(update)
+        who_wins(update, game_board)
 
 def check(update: Update):
     global game_board
@@ -45,16 +49,19 @@ def bot_turn(update: Update):
             update.message.reply_text(f"------------------- \n | {game_board[0]} | {game_board[1]} | {game_board[2]} | \n | {game_board[3]} | {game_board[4]} | {game_board[5]} | \n | {game_board[6]} | {game_board[7]} | {game_board[8]} | \n ------------------- ")
 
 def who_wins(update: Update, game_board):
+    win = False
     if len(used_cell) < 9:
         win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
         for i in win_coord:
             if game_board[i[0]] == game_board[i[1]] == game_board[i[2]]:
                 update.message.reply_text(f"Победил игрок {game_board[i[0]]}. Сыграем еще? /start_game")
-                quit()
+                win = True
     elif len(used_cell) == 9:
         win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
         for i in win_coord:
             if game_board[i[0]] == game_board[i[1]] == game_board[i[2]]:
                 update.message.reply_text(f"Победил игрок {game_board[i[0]]}. Сыграем еще? /start_game ")
-                quit()
-        update.message.reply_text("Ничья. Сыграем еще? /start_game")
+                win = True
+        if not win:
+            update.message.reply_text("Ничья. Сыграем еще? /start_game")
+    return win
